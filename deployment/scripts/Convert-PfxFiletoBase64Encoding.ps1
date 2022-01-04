2eec13fa7ef5e4cdd4523ccee4401248e4515b87
Brian Denicola <brian@briandenicola.com>
2022-01-04 13:41:40 -0600

    Added Windows Powershell 5.1 support

--- deployment/scripts/Convert-PfxFiletoBase64Encoding.ps1
+++ deployment/scripts/Convert-PfxFiletoBase64Encoding.ps1
@@ -1,6 +1,23 @@
 param(
     [Parameter(Mandatory=$true)]
     [ValidateScript({Test-Path -Path $_})]
-    [string] $CertPath
+    [string] $CertPath,
+
+    [switch] $CopyToClipboard
 )
-return [convert]::ToBase64String( (Get-Content -AsByteStream -Path $CertPath) )
+
+
+if( $host.Version -ge [Version]::New(7,0,0) ) {
+    $data = Get-Content -AsByteStream -Path $CertPath
+} else {
+    $data = Get-Content -Encoding byte -Raw -Path $CertPath
+}
+
+$pfx = [convert]::ToBase64String($data)
+
+if($CopyToClipboard) {
+    $pfx | Set-Clipboard
+}
+
+return $pfx
+
